46ab0b1ebe7e6460af84aafecf7e828869dd5eee
Samuel Torres <sam.torres@smiledirectclub.com>
2022-12-23 10:13:00 -0800

    Configure nginx ingress service and serve up test auth service content

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,5 +4,9 @@ const app = express();
 // Parse json from req bodies
 app.use(express.json());
 
+app.get("/api/users/currentUser", (req, res) => {
+  res.send("test test test");
+});
+
 // Will eventually be replaced by kubernates
 app.listen(4000, () => console.log("Auth service listening on port 4000"));
