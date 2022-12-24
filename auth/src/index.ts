a0ab8c55fa7c63439ffa91699e0e1cb56b3779a9
Samuel Torres <sam.torres@smiledirectclub.com>
2022-12-24 10:48:18 -0800

    Import and use current user router in index.ts auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
+import { currentUserRouter } from "./routes/currentUser";
 
 const app = express();
 // Parse json from req bodies
 app.use(express.json());
 
-app.get("/api/users/currentUser", (req, res) => {
-  res.send("test test test");
-});
+// Use imported route handlers
+app.use(currentUserRouter);
 
 // Will eventually be replaced by kubernates
 app.listen(4000, () => console.log("Auth service listening on port 4000"));
