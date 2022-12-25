84115977d00679733d0dafc39b43e048db548450
Samuel Torres <sam.torres@smiledirectclub.com>
2022-12-25 07:55:39 -0800

    Update route path to have correct casing

--- auth/src/routes/signUp.ts
+++ auth/src/routes/signUp.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/api/users/signUp", async (req, res) => {
+router.post("/api/users/signup", async (req, res) => {
   res.send("you have been signed up");
 });
 
