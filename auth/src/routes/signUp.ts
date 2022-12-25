c8084e0a0bd24528d3671c340d545d98ddd4d8f3
Samuel Torres <sam.torres@smiledirectclub.com>
2022-12-25 08:08:04 -0800

    Use body validation from express validator for auth sign up route

--- auth/src/routes/signUp.ts
+++ auth/src/routes/signUp.ts
@@ -1,12 +1,23 @@
 import express from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", async (req, res) => {
-  // Grab user creds from body
-  const { email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 chars")
+  ],
+  async (req, res) => {
+    // Grab user creds from body
+    const { email, password } = req.body;
 
-  // Validation
-});
+    // Validation
+  }
+);
 
 export { router as signUpRouter };
