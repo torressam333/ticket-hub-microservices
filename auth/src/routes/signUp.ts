76b0342848e35cbf305ddbe06a6ff23734e6dbf6
Samuel Torres <sam.torres@smiledirectclub.com>
2022-12-26 16:43:01 -0700

    Update validation in auth service and update start script to poll for changes

--- auth/src/routes/signUp.ts
+++ auth/src/routes/signUp.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -9,14 +9,22 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
-      .isLength({ min: 4, max: 20 })
+      .isLength({ min: 6, max: 20 })
       .withMessage("Password must be between 4 and 20 chars")
   ],
   async (req: Request, res: Response) => {
+    // May contain errors
+    const errors = validationResult(req);
+
+    // Send potential erros in response
+    if (!errors.isEmpty()) return res.status(400).send(errors.array());
+
     // Grab user creds from body
     const { email, password } = req.body;
 
-    // Send potential erros back to user
+    console.log("creating user....");
+
+    res.send({});
   }
 );
 
