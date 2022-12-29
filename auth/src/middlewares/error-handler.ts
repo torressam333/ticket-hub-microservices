dc916aa8d753e6435d305317d35e586f54530617
Samuel <torressam333@gmail.com>
2022-12-28 17:29:35 -0700

    Differentiate error types and return common error structure

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -9,14 +9,23 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    console.log('Handling rve error');
+    // Extract properties from ValidationError in RVE class
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+
+    // Return object with standardized format as {errors: [{}]}
+    return res.status(400).send({ errors: formattedErrors });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    console.log('DB error');
+    // Same standardized error format
+    return res.status(503).send({
+      // Reason exists in DCE class as a property
+      errors: [{ message: err.reason }],
+    });
   }
 
-  res.status(400).json({
-    message: err.message,
-  });
+  // Fallback generic error
+  res.status(400).send({ errors: [{ message: err.message }] });
 };
