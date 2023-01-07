f34238d1bbd59427d99ae67f528189531592f5b4
Samuel <torressam333@gmail.com>
2023-01-07 08:47:53 -0700

    Update user model to implement specific interfaces

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,14 +1,24 @@
 import mongoose from 'mongoose';
 
-// Interface to describe properties required for new User
+// Interface to describe attributes required for a new individual User
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-// Interface to descrive props fir User Model
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+// Interface to descrive props/shape of the entire User Model Collection
+// Along with any addtional methods the User model should have
+interface UserModel extends mongoose.Model<UserDoc> {
+  // must return instance of UserDoc
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface to descrive properties that the User Document has
+// Inherit from mongoose base Document class + extra custom props to add
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  // Any additional properties go here
 }
 
 const userSchema = new mongoose.Schema({
@@ -23,11 +33,12 @@ const userSchema = new mongoose.Schema({
 });
 
 // Way to implement TS interface with mongoose model via custom function
-// Now we can call: User.build({})
+// Now we can call: User.build({}) since custom fn was added to statics list
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+// Init user model w/ interfaces implemented
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
