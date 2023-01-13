f75361455ac1694b025b0090b6f3c4dd573678de
Samuel <torressam333@gmail.com>
2023-01-13 09:59:29 -0700

    Start working on signin route and generalize request validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/RequestValidationError';
 
 const signInRouter = express.Router();
 
-signInRouter.post('/api/users/signin', async (req, res) => {
-  res.send('signed in');
-});
+const validationArr = [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('Invalid password'),
+];
+
+signInRouter.post(
+  '/api/users/signin',
+  validationArr,
+  async (req: Request, res: Response) => {
+    // May contain errors
+    const errors = validationResult(req);
+
+    // Send potential erros in response
+    if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
+  }
+);
 
 export default signInRouter;
