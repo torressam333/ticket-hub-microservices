fb779a9cf60cff0a719347a08106b0d34ad1db5e
Samuel <torressam333@gmail.com>
2023-01-15 09:57:41 -0700

    Add currentUser endpoint to give f/e a way to confirm a user is logged in

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,23 @@
+/**
+ * Determine if a there is a currently signed in user
+ * w/ a valid jwt (cookie)
+ */
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const currentUserRouter = express.Router();
 
 currentUserRouter.get('/api/users/currentUser', async (req, res) => {
-  res.send('testing testing');
+  // // Invalid user/jwt - bail
+  if (!req.session?.jwt) return res.status(401).json({ currentUser: null });
+  // If token was altered, an err will be thrown - catch it.
+  try {
+    // Verify jwt and grab data from jwt
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.status(200).json({ currentUser: payload });
+  } catch (err) {
+    res.json({ currentUser: null });
+  }
 });
 
 export default currentUserRouter;
