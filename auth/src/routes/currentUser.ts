221cbe570d7c11f0b6ab7687f2944d0c36d475b5
Samuel <torressam333@gmail.com>
2023-01-16 11:37:43 -0700

    Extract logic of retrieving current user to a mw function

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -4,20 +4,17 @@
  */
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const currentUserRouter = express.Router();
 
-currentUserRouter.get('/api/users/currentUser', async (req, res) => {
-  // // Invalid user/jwt - bail
-  if (!req.session?.jwt) return res.status(401).json({ currentUser: null });
-  // If token was altered, an err will be thrown - catch it.
-  try {
-    // Verify jwt and grab data from jwt
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.status(200).json({ currentUser: payload });
-  } catch (err) {
-    res.json({ currentUser: null });
+currentUserRouter.get(
+  '/api/users/currentUser',
+  currentUser,
+  async (req, res) => {
+    // Return res w/ currentUser off of req (set in mw)
+    res.status(200).json({ currentUser: req.currentUser || null });
   }
-});
+);
 
 export default currentUserRouter;
