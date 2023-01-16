dfc5943e274b6e5ed0f2fa6694e92a9707c740d8
Samuel <torressam333@gmail.com>
2023-01-16 11:01:02 -0700

    Implement user sign out from auth service

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express from 'express';
 const signoutRouter = express.Router();
 
 signoutRouter.post('/api/users/signout', async (req, res) => {
-  res.send('signed out');
+  // Empty cookie and session info
+  req.session = null;
+
+  res.send({});
 });
 
 export default signoutRouter;
