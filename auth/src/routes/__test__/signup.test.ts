982abb7d9b296439dd41c566a464297e13fab834
Samuel <torressam333@gmail.com>
2023-01-18 10:20:37 -0700

    Add some basic unit tests for sign up route

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,42 @@ it('returns 201 on successful sign up', async () => {
     })
     .expect(201);
 });
+
+it('returns 400 status code with invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({ email: 'fakeemail', password: 'Password123' })
+    .expect(400);
+});
+
+it('returns 400 status code with invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: 'Pass' })
+    .expect(400);
+});
+
+it('returns 400 status code with missing input values', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({ email: '', password: '' })
+    .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'Test123',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'Test123456',
+    })
+    .expect(400);
+});
