834bbde774816de4530b9bcc2bcdcc3ce3e6f36b
Samuel <torressam333@gmail.com>
2023-01-24 19:35:02 -0700

    Build out the sign up component

--- tickethub-client/next.config.js
+++ tickethub-client/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   webpack: (config) => {
-    config.watchOptions.poll = 300;
+    config.watchOptions.poll = 200;
 
     return config;
   },
--- tickethub-client/pages/auth/signup.js
+++ tickethub-client/pages/auth/signup.js
@@ -1,25 +1,62 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const signUpSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+
   return (
-    <form className='container pt-4'>
+    <form className='container pt-4' onSubmit={signUpSubmit}>
       <h1>Sign Up</h1>
-      <div class='form-group'>
-        <label for='exampleInputEmail1'>Email address</label>
+      <div className='form-group'>
+        <label htmlFor='email'>Email address</label>
         <input
           type='email'
-          class='form-control'
+          id='email'
+          className='form-control'
           aria-describedby='emailHelp'
           placeholder='Enter email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
-      <div class='form-group'>
-        <label for='password'>Password</label>
-        <input type='password' class='form-control' placeholder='Password' />
+      <div className='form-group'>
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          className='form-control'
+          id='password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      <button type='submit' class='btn btn-primary mt-2'>
-        Sign Up
-      </button>
+      {errors.length > 0 && (
+        <div className='alert alert-danger'>
+          <h4>Something went wrong:</h4>
+          <ul className='my-0'>
+            {errors &&
+              errors.map((err) => <li key={err.message}>{err.message}</li>)}
+          </ul>
+        </div>
+      )}
+      <button className='btn btn-primary mt-2'>Sign Up</button>
     </form>
   );
 };
