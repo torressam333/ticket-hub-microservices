7c26dcbc743a05c794bea0e2400490c155294b87
Samuel <torressam333@gmail.com>
2023-01-26 19:54:52 -0700

    Start adding initial props to grab data on ssr process

--- tickethub-client/pages/index.js
+++ tickethub-client/pages/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useRequest } from '../hooks/useRequest';
 
-const index = () => {
-  return <div>Landing Page, update!</div>;
+// const { executeRequest, errors } = useRequest({
+//   url: '/api/users/currentUser',
+//   method: 'get',
+// });
+
+const LandingPage = ({ message }) => {
+  return <div>{message ? message : ' You are not signed in'}</div>;
 };
 
-export default index;
+// Fetch data during SSR process
+LandingPage.getInitialProps = async () => {
+  console.log('I am on the server');
+
+  return { message: '' };
+  // const response = await executeRequest();
+  // console.log({ response });
+
+  // if (response.currentUser) return { currentUser: response.currentUser };
+};
+
+export default LandingPage;
