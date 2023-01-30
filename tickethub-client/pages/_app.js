3f97a0c0277fb265d0688fc7d90d7d20c73570e2
Samuel <torressam333@gmail.com>
2023-01-30 09:31:06 -0700

    Start moving getInitialProps into _app.js

--- tickethub-client/pages/_app.js
+++ tickethub-client/pages/_app.js
@@ -1,5 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/buildClient';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h2>Header</h2>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
+
+// Fetch data during SSR process
+AppComponent.getInitialProps = async ({ ctx }) => {
+  let currentUser;
+  try {
+    const { data } = await buildClient(ctx).get('/api/users/currentUser');
+
+    console.log('HERE');
+    console.log(data);
+
+    currentUser = data;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return currentUser;
+};
+
+export default AppComponent;
