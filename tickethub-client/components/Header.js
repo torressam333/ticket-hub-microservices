83fb7eaa63b821d3d704c5b14094f48eb71afe5d
Samuel <torressam333@gmail.com>
2023-01-31 08:51:41 -0700

    Proper conditional logic for global header links

--- tickethub-client/components/Header.js
+++ tickethub-client/components/Header.js
@@ -2,19 +2,27 @@ import React from 'react';
 import Link from 'next/link';
 
 const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className='nav-item'>
+          <Link href={href}>{label}</Link>
+        </li>
+      );
+    });
+
   return (
     <nav className='navbar navbar-light bg-light'>
       <Link href='/' className='navbar-brand'>
         Tickethub
       </Link>
       <div className='d-flex justify-content-end'>
-        <ul className='nav d-flex align-items-center'>
-          {currentUser ? (
-            <Link href='/'>Sign Out</Link>
-          ) : (
-            <Link href='/auth/signup'>Sign In</Link>
-          )}
-        </ul>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
       </div>
     </nav>
   );
