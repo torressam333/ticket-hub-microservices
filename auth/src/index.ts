e370516cacde08d316faed1bb0872c0a802ed7dc
Samuel <torressam333@gmail.com>
2023-02-08 09:37:39 -0700

    Move mongo uri to depl yaml files for auth and ticketing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,12 +5,13 @@ import mongoose from 'mongoose';
 const start = async () => {
   // Check for required env vars
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY is undefined');
+  if (!process.env.MONGO_URI) throw new Error('Mongo uri is undefined');
   try {
     // Added per warning about false being default in mongo v7
     mongoose.set('strictQuery', true);
 
     // Auth mongo-srv cluster ip service ip address needed (kubectl get services)
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log('Mongo auth srv connected properly...');
   } catch (error) {
--- ticketing/src/index.ts
+++ ticketing/src/index.ts
@@ -5,12 +5,14 @@ import mongoose from 'mongoose';
 const start = async () => {
   // Check for required env vars
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY is undefined');
+  if (!process.env.MONGO_URI) throw new Error('Mongo uri is undefined');
+
   try {
     // Added per warning about false being default in mongo v7
     mongoose.set('strictQuery', true);
 
     // Ticketing mongo-srv cluster ip service ip address needed (kubectl get services)
-    await mongoose.connect('mongodb://ticketing-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log('Ticketing mongo srv connected properly...');
   } catch (error) {
