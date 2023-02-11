fb1b27d91ff0b2f83e126e739868b8fbc0ec30c4
Samuel <torressam333@gmail.com>
2023-02-11 08:32:17 -0700

    Only valid post creation test remains for new test file

--- ticketing/src/routes/new.ts
+++ ticketing/src/routes/new.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@torressam/common';
+import { requireAuth, validateRequest } from '@torressam/common';
+import { body } from 'express-validator';
 
 const newTicketRouter = express.Router();
 
 newTicketRouter.post(
   '/api/tickets',
   requireAuth,
+  [body('title').notEmpty().withMessage('Title is required')],
+  [
+    body('price')
+      .notEmpty()
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
   (req: Request, res: Response) => {
     try {
       res.sendStatus(200);
