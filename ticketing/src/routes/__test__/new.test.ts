9314cea41d33a4814ed632c337b1eb29db44e306
Samuel <torressam333@gmail.com>
2023-02-11 08:18:45 -0700

    Finish new ticket auth request test

--- ticketing/src/routes/__test__/new.test.ts
+++ ticketing/src/routes/__test__/new.test.ts
@@ -13,7 +13,10 @@ describe('New Ticket', () => {
   });
 
   it('returns a status other than 401 if user is signed in', async () => {
-    const response = await request(app).post('/api/tickets').send({});
+    const cookie = signup();
+    const response = await request(app)
+      .post('/api/tickets')
+      .set('Cookie', cookie); // adds auth header.send({});
 
     expect(response.status).not.toEqual(401);
   });
--- ticketing/src/test/setup.ts
+++ ticketing/src/test/setup.ts
@@ -1,12 +1,14 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import { Buffer } from 'node:buffer';
 
 // Declare in global setup scope
 let mongo: any;
 
 // No need to exp. import in other test files
 declare global {
-  function signup(): Promise<string[]>;
+  function signup(): string[];
 }
 
 // Added per warning about false being default in mongo v7
@@ -37,13 +39,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signup = async () => {
+global.signup = () => {
   // Build jwt payload {id, email}
-  // Create jwt
-  // Build session object
-  // Convert session to JSON
-  // Encode JSON as base 64
-  // Return mock cooke with encoded data
+  const payload = {
+    id: '16dkjw523122',
+    email: 'test@test.com',
+  };
 
+  // Create jwt
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object
+  const session = { jwt: token };
+
+  // Convert session to JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Encode JSON as base 64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // Return mock cooke with encoded data
   return [`session=${base64}`];
 };
