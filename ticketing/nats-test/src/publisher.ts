4f24693d0b76c3a9faacf5f051fdbe43698cb40f
Samuel <torressam333@gmail.com>
2023-02-25 08:11:37 -0700

    Start building out pub sub with nats streaming server

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -1,3 +1,14 @@
 /**
  * Dedicated to be the publisher of this pub sub
  */
+import nats from 'node-nats-streaming';
+
+// Create "client" to connect to streaming server
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+// Wait for client to successfully connect to n.s.s via "connect" emitted event
+stan.on('connect', () => {
+  console.log('pub is connected to nats');
+});
