3fc27a95812c7b8213cde2e00740adf12851652d
Samuel <torressam333@gmail.com>
2023-03-15 06:46:25 -0700

    Create abstract class to replace verbose nats connection and listener

--- ticketing/nats-test/src/subscriber.ts
+++ ticketing/nats-test/src/subscriber.ts
@@ -1,7 +1,7 @@
 /**
  * Dedicated to be a listener for events of pub-sub
  */
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 // Remove metadata from cli output
@@ -51,3 +51,51 @@ stan.on('connect', () => {
 // Watch for interrupt or terminate signal. Incercept these req's and close the connection in node nats server.
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+abstract class Subscriber {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait) // Only wait 5 seconds to retry events
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      // Quick indicator for message events
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      // Parse data from event
+      const parsedData = this.parseMessage(msg);
+
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+
+    // Add ability to parse string or buffer
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}
