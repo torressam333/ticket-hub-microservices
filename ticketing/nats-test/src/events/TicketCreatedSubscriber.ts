dfcd5efcc6eecf76a4d8caf0e1cee92fe74b1274
Samuel <torressam333@gmail.com>
2023-03-20 07:17:54 -0700

    Enfore strict type checking on onmessage data for ticket created

--- ticketing/nats-test/src/events/TicketCreatedSubscriber.ts
+++ ticketing/nats-test/src/events/TicketCreatedSubscriber.ts
@@ -7,7 +7,7 @@ export class TicketCreatedSubscriber extends Subscriber<TicketCreatedEvent> {
   readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service-queue-group';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data', data);
 
     // If event is properly received
