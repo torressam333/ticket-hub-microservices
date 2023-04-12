6ccb69e905049272f217be8addfd79c48410538d
Samuel <torressam333@gmail.com>
2023-04-11 21:12:20 -0700

    Remove res send for new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@torressam/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const newOrderRouter = express.Router();
 
-newOrderRouter.post('/api/orders', async (req: Request, res: Response) => {
-  res.send('New new new');
-});
+newOrderRouter.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketID is required'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {}
+);
 
 export default newOrderRouter;
