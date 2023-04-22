5c1c7eaf4ded88c76045d0448f07f21e1c34a5e5
Samuel <torressam333@gmail.com>
2023-04-22 08:43:47 -0700

    Add clarifying comments for each enum case for order statuses

--- common/src/events/types/order-status.ts
+++ common/src/events/types/order-status.ts
@@ -1,6 +1,11 @@
 export enum OrderStatus {
+  // When order has been created but ticket has not been reserved
   Created = 'created',
+  // When the ticket has already been reserved or explicitelt cancelled by user
+  // or if order expires before payment
   Cancelled = 'cancelled',
+  // When order has successfully reserved ticket
   AwaitingPayment = 'awaiting:payment',
-  Complete = 'complete'
+  // When order has reserved ticket and user pays
+  Complete = 'complete',
 }
