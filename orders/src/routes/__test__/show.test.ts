b63777b60581970ec5197637edef464a7f1faf8b
Samuel <torressam333@gmail.com>
2023-05-01 08:02:01 -0700

    Add test for unauthorized order fetching

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -34,4 +34,32 @@ describe('Orders Service', () => {
     expect(response.body.ticket.title).toEqual(ticket.title);
     expect(response.body.id).toEqual(order.id);
   });
+
+  it('returns error if one user tries fetch another users order ', async () => {
+    // create a ticket
+    const ticket = Ticket.build({
+      title: 'Some Ticket Title',
+      price: 100,
+    });
+
+    await ticket.save();
+
+    // Generate mock mongoose id
+    // const ticketId = new mongoose.Types.ObjectId();
+    const user = global.signup();
+    const user2 = global.signup();
+
+    // make a request to build an order with this ticket
+    const { body: order } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', user)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+    // // make a request to fetch the order with DIFFERENT user
+    await request(app)
+      .get(`/api/orders/${order.id}`)
+      .set('Cookie', user2)
+      .expect(401);
+  });
 });
