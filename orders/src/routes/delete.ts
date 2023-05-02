00fe35ce0865d5d1813f453ff0b3455451df601d
Samuel <torressam333@gmail.com>
2023-05-02 06:53:54 -0700

    Write out route handler logic for order deletion/cancellation

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,11 +1,32 @@
 import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  NotFoundError,
+  UnauthorizedError,
+} from '@torressam/common';
+import Order, { OrderStatus } from '../models/order';
 
 const deleteOrderRouter = express.Router();
 
 deleteOrderRouter.delete(
   '/api/orders/:orderId',
+  requireAuth,
   async (req: Request, res: Response) => {
-    res.send('delete delete delete');
+    // Find specific order
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId).populate('ticket');
+
+    // Order not found
+    if (!order) throw new NotFoundError();
+
+    // Incorrect user trying to access this order
+    if (order.userId !== req.currentUser!.id)
+      throw new UnauthorizedError('Not authorized to view order');
+
+    // Update order status to be cancelled (soft delete-esque)
+    order.status = OrderStatus.Cancelled;
+
+    res.status(204).send(order);
   }
 );
 
