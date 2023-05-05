0670998f984b925a763f2a9878f8a0fad36891db
Samuel <torressam333@gmail.com>
2023-05-05 07:34:23 -0700

    Build ticket created subscriber class

--- orders/src/events/subscribers/ticket-created-subscriber.ts
+++ orders/src/events/subscribers/ticket-created-subscriber.ts
@@ -2,3 +2,10 @@ import { natsWrapper } from '../../NatsWrapper';
 import { Message } from 'node-nats-streaming';
 import { Subjects, Subscriber, TicketCreatedEvent } from '@torressam/common';
 import Ticket from '../../models/ticket';
+
+export class TicketCreatedSubscriber extends Subscriber<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = 'orders-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {}
+}
