2fcd4727dae7018b7830a261a059bee304298078
Samuel <torressam333@gmail.com>
2023-05-07 15:40:17 -0700

    Minor optimization and use constant for queue group name

--- orders/src/events/subscribers/ticket-created-subscriber.ts
+++ orders/src/events/subscribers/ticket-created-subscriber.ts
@@ -2,10 +2,11 @@ import { natsWrapper } from '../../NatsWrapper';
 import { Message } from 'node-nats-streaming';
 import { Subjects, Subscriber, TicketCreatedEvent } from '@torressam/common';
 import Ticket from '../../models/ticket';
+import { queueGroupName } from './queueGroupName';
 
 export class TicketCreatedSubscriber extends Subscriber<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
     // TO do pass up data
