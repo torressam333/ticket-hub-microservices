8fecda6b591f7806a622924ef2c1a80b9ec4ec79
Samuel <torressam333@gmail.com>
2023-05-08 08:17:05 -0700

    Add the logic for ticket updated subscriber in order service

---
+++ orders/src/events/ticket-updated-subscriber.ts
@@ -0,0 +1,34 @@
+import { Message } from 'node-nats-streaming';
+import {
+  NotFoundError,
+  Subjects,
+  Subscriber,
+  TicketUpdatedEvent,
+} from '@torressam/common';
+import Ticket from '../models/ticket';
+import { queueGroupName } from './subscribers/queueGroupName';
+
+export class TicketUpdatedSubscriber extends Subscriber<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) throw new NotFoundError();
+
+    const { title, price } = data;
+
+    // Update title and price
+    ticket.set({
+      title,
+      price,
+    });
+
+    // Save updated ticket
+    await ticket.save();
+
+    // Acknowledge message for nats to process event
+    msg.ack();
+  }
+}
