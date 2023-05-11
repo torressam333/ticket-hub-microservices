f66db292c5a8fc44420ce35af430abc44fe62381
Samuel <torressam333@gmail.com>
2023-05-11 07:39:53 -0700

    Finish test for ticket model to test concurrency issues

--- ticketing/src/models/__test__/ticket.test.ts
+++ ticketing/src/models/__test__/ticket.test.ts
@@ -1,3 +1,35 @@
-import { Ticket } from '../ticket';
+import Ticket from '../ticket';
 
-it('implements optimistic concurrency control by version number', () => {});
+it('implements optimistic concurrency control by version number', async () => {
+  // Create instance of ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 100,
+    userId: '123',
+  });
+
+  // Save to db (will have version # assigned to ticket)
+  await ticket.save();
+
+  // Fetch same ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make 2 separate changes to fetched tickets
+  firstInstance!.set({ title: 'Snoop Dogg Concert' });
+  secondInstance!.set({ title: 'Logic Concert' });
+
+  // save first fetched ticket w/ change and updated version #
+  await firstInstance!.save();
+
+  // Save 2nd ticket (w. outdated version #) and expect error
+  try {
+    await secondInstance!.save();
+  } catch (error) {
+    return;
+  }
+
+  // Work around test to ensure it doesnt go past try catch. If we see
+  // this thrown error, its a failing
+  throw new Error('Should not reach this point');
+});
