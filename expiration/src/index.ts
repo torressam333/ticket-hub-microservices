e2e6d80085e73efa9939dd90d227fbd8aae611d6
Samuel <torressam333@gmail.com>
2023-05-25 08:42:19 -0700

    Clean up expiration service index.ts

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,14 +1,8 @@
-import app from './app';
-import mongoose from 'mongoose';
 import { natsWrapper } from './NatsWrapper';
-import { OrderCreatedSubscriber } from './events/subscribers/OrderCreatedSubscriber';
-import { OrderCancelledSubscriber } from './events/subscribers/OrderCancelledSubscriber';
 
 // Mongoose connect fn
 const start = async () => {
   // Check for required env vars
-  if (!process.env.JWT_KEY) throw new Error('JWT_KEY is undefined');
-  if (!process.env.MONGO_URI) throw new Error('Mongo uri is undefined');
   if (!process.env.NATS_CLUSTER_ID)
     throw new Error('NATS cluster id undefined');
   if (!process.env.NATS_CLIENT_ID)
@@ -34,25 +28,10 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
-    // Begin listening for order created/cancelled events
-    new OrderCreatedSubscriber(natsWrapper.client).listen();
-    new OrderCancelledSubscriber(natsWrapper.client).listen();
-
-    // Added per warning about false being default in mongo v7
-    mongoose.set('strictQuery', true);
-
-    // Ticketing mongo-srv cluster ip service ip address needed (kubectl get services)
-    await mongoose.connect(process.env.MONGO_URI);
-
     console.log('Ticketing mongo srv connected properly...');
   } catch (error) {
     console.log('Something went wrong: ', error);
   }
-
-  // K8S listening on port 3000 (@see auth-depl.yaml file)
-  app.listen(3000, async () =>
-    console.log('Ticketing service listening on port 3000')
-  );
 };
 
 // Start app + other dependent start up code
