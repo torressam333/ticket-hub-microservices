5dbf9d29a629d94599aee9423b3a142f4537945d
Samuel <torressam333@gmail.com>
2023-05-26 07:31:30 -0700

    Make changes to expiration infra files to make skaffold run properly

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,38 +1,31 @@
 import { natsWrapper } from './NatsWrapper';
 
-// Mongoose connect fn
 const start = async () => {
-  // Check for required env vars
-  if (!process.env.NATS_CLUSTER_ID)
-    throw new Error('NATS cluster id undefined');
-  if (!process.env.NATS_CLIENT_ID)
-    throw new Error('NATS client id is undefined');
-  if (!process.env.NATS_URL) throw new Error('NATS url is undefined');
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
 
-  // Pull from ticketing depl file in k8s
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
-
     natsWrapper.client.on('close', () => {
-      console.log('NATS Connection Closed');
-
+      console.log('NATS connection closed!');
       process.exit();
     });
-
-    // Handle the stop/close/restar ting of any downstream subscribers
-    // Watch for interrupt or terminate signal. Incercept these req's and close the connection in node nats server
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
-
-    console.log('Ticketing mongo srv connected properly...');
-  } catch (error) {
-    console.log('Something went wrong: ', error);
+  } catch (err) {
+    console.error(err);
   }
 };
 
-// Start app + other dependent start up code
 start();
