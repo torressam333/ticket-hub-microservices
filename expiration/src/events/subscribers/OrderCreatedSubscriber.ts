1d9d3301c1969e9ed85f0f94e2dc3d042b761f56
Samuel <torressam333@gmail.com>
2023-05-29 09:18:15 -0700

    Wire up correct delay for ticket and order processing via events

--- expiration/src/events/subscribers/OrderCreatedSubscriber.ts
+++ expiration/src/events/subscribers/OrderCreatedSubscriber.ts
@@ -13,10 +13,20 @@ export class OrderCreatedSubscriber extends Subscriber<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Difference in time between expires at and now.
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    console.log('waiting this many ms to process job ', delay);
+
     // Create new job and queue it up
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
 
     msg.ack();
   }
