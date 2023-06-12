12ea236f682d28b25fe96347bcdaddaa51b3888d
Samuel <torressam333@gmail.com>
2023-06-12 07:50:46 -0700

    Comment out mock way of testing stripe api in order to test by making real calls to stripe

--- payments/src/__mocks__/stripe.ts
+++
@@ -1,7 +0,0 @@
-// Implement mock stripe api client so real stripe doesn't need to be running
-export const stripe = {
-  charges: {
-    // Return promise that auto resolves as stripes charges.create method
-    create: jest.fn().mockResolvedValue({}),
-  },
-};
