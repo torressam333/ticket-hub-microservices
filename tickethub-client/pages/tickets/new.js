83d5d52d37482c6619afa192c6e0f98c9e2f7db8
Samuel <torressam333@gmail.com>
2023-06-17 12:08:36 -0700

    Add form to new ticket react component for tickets

--- tickethub-client/pages/tickets/new.js
+++ tickethub-client/pages/tickets/new.js
@@ -1,3 +1,37 @@
 /**React component to create a new ticket */
+const NewTicket = () => {
+  return (
+    <div className='container-xxl'>
+      <h1>Create a Ticket</h1>
+      <form>
+        <div class='mb-3'>
+          <label for='title' class='form-label'>
+            Title
+          </label>
+          <input
+            type='text'
+            class='form-control'
+            id='title'
+            placeholder='Enter a title'
+          />
+        </div>
+        <div class='mb-3'>
+          <label for='price' class='form-label'>
+            Price
+          </label>
+          <input
+            type='price'
+            class='form-control'
+            id='price'
+            placeholder='Enter a price'
+          />
+        </div>
+        <button type='submit' class='btn btn-primary'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-const newTicket = () => { };
+export default NewTicket;
