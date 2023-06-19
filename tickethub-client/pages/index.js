0406eea905944d16367873fbfb285578c9ff040a
Samuel <torressam333@gmail.com>
2023-06-19 07:27:51 -0700

    Display all tickets for sale on homepage

--- tickethub-client/pages/index.js
+++ tickethub-client/pages/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
-const LandingPage = ({ currentUser }) => {
-  return !currentUser ? (
-    <h1>You are not signed in</h1>
-  ) : (
-    <h1>You are signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
 // Fetch data during SSR process
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  // Request to ticket service to get all tickets
+  const { data } = await client.get('/api/tickets');
+
+  // Gets merged into props being passed to LandingPage component above
+  return { tickets: data };
 };
 
 export default LandingPage;
