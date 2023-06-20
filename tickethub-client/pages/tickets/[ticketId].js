d2136051b13cc70b377d13313533acaa3400aab2
Samuel <torressam333@gmail.com>
2023-06-20 08:08:50 -0700

    Make api call in getinitial props to grab ticket info for ticket show component

--- tickethub-client/pages/tickets/[ticketId].js
+++ tickethub-client/pages/tickets/[ticketId].js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const TicketShow = (props) => {
+const TicketShow = ({ ticket }) => {
   return <div>TicketShow</div>;
+};
+
+// Grab props needed to display in component
+TicketShow.getInitialProps = async (context, client) => {
+  // Grab ticket id from url (matches filename for query parm [ticketId])
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  // merge into all diff props from the TicketShow comp above
+  return { ticket: data };
 };
 
 export default TicketShow;
