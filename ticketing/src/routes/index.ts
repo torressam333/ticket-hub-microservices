d222571046877e241dcf1fc398409f79002a81dc
Samuel <torressam333@gmail.com>
2023-06-23 07:22:42 -0700

    Modify query in tickets service to return only tickets available for purchase

--- ticketing/src/routes/index.ts
+++ ticketing/src/routes/index.ts
@@ -6,7 +6,9 @@ const indexTicketRouter = express.Router();
 indexTicketRouter.get('/api/tickets', async (req: Request, res: Response) => {
   try {
     // TODO: Add filtering based on availability
-    const tickets = await Ticket.find({});
+    const tickets = await Ticket.find({
+      orderId: undefined,
+    });
 
     res.status(200).json(tickets);
   } catch (error) {}
